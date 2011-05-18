d7608e159e46f1bfc4ae4f90e53ee139effa939c
shuerhaaken <none@none>
2011-05-18 14:55:46 +0200

    add script for manual po file update from pot

---
+++ po/update_po_from_pot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script can be used to manually update po files from a pot file
+
+if [ -f ./xnoise.pot ] ; then
+	echo "found pot file!"
+else
+	echo "no pot file found"
+	exit 1
+fi
+
+for file in ./*.po ; do
+	msgmerge -U $file xnoise.pot
+done
