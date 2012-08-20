de56bfebb485244f932a210dc98cfed29063c7bf
Tal Hadad <tal_hd@hotmail.com>
2012-08-20 13:12:23 +0200

    update debian package creation script

--- package_chef.sh
+++ package_chef.sh
@@ -1,11 +1,11 @@
-# Script for debian packagement by Tal Hadad.
+# Script for debian packaging by Tal Hadad.
 # You can do what ever you want with this file under
 # your responsibility, no copyright intended.
 
 #The format is:
 #ARGS_INPUT_FORMAT="OPTION STRING 1|OPTION STRING 2(OPTIONAL)|OPTION STRING N(OPTIONAL):VARIABLE NAME WHICH HOLDS OPTION VALUE:DEFAULT OPTION VALUE(OPTIONAL):DEFAULT OPTION VALUE IF USED AS SWITCH(OPTIONAL)"
 
-ARGS_INPUT_FORMAT="-h|-?|--help:help:-1:0,--name:name:package,--version:version:1.0,--src-tar:src_tar:-1,--src-dir:src_dir:${PWD},--output-dir:output_dir:package_result,--distro:distro:debian,-S|--build-source:build_source:no:yes,-B|--build-binary:build_binary:no:yes,--maintainer:maintainer:-1,-us|--dont-sign:dont_sign:no:yes,-uc|--dont-sign-changes:dont_sign_changes:no:yes,-sa|--include-orig:include_orig:yes:yes"
+ARGS_INPUT_FORMAT="-h|-?|--help:help:-1:0,--name:name:package,--version:version:1.0,--src-tar:src_tar:-1,--src-dir:src_dir:${PWD},--output-dir:output_dir:package_result,--distro:distro:debian,-S|--build-source:build_source:no:yes,-B|--build-binary:build_binary:no:yes,--maintainer:maintainer:-1,-us|--dont-sign:dont_sign:no:yes,-uc|--dont-sign-changes:dont_sign_changes:no:yes,-sa|--include-orig:include_orig:yes:yes,-sd:dont_include_orig:no:yes"
 
 # Parsing arguments script.
 # This script was taken from:
@@ -63,25 +63,37 @@ Options:
   --src-dir=DIR                The directory of the source package, if no
                                  TARBALL was given. (default: $\{PWD\})
   --output-dir=OUT_DIR         The output folder. (default: package_result)
-  --distro=DISTRO              The targeted distro for the packge. (default:
-                                 debian; for Ubuntu use 'ubuntu/precise' or
-                                 'ubuntu/quantal')
+  --distro=DISTRO              The targeted distro for the packge. e.g.:
+                                 debian, ubuntu/precise, ubuntu/quantal
+                                 (default: debian)
   -S, --build-source[=yes|no]  Weather to only build a Debian source package.
   -B, --build-binary[=yes|no]  Weather to only build Debian binary packages.
   --maintainer=MAINTAINER      The maintainer which sign on the packages.
-  -us, --dont-sign[=yes|no]    Dont create a GPG signature file(.dsc) for the
+  -us, --dont-sign[=yes|no]    Don't create a GPG signature file(.dsc) for the
                                  packages. (default: no)
-  -uc,                         Dont sign the .changes file. (default: no)
+  -uc,                         Don't sign the .changes file. (default: no)
   --dont-sign-changes[=yes|no]
   -sa, --include-orig[=yes|no] Include the package original source file in the
-                                 .changes file. (default: no)
+                                 .changes file. (default: yes)
+  -sd                          Don't include the package original source file
+                                 in the .changes file. (default: no)
   
   
-Xnoise EXAMPLE:
-  Source package tar.gz is in the same folder as this script.
+Xnoise 0.2.5 EXAMPLE:
+  Source package tar.gz is in current working directory(\$PWD).
   
-  ./package_chef.sh --distro=ubuntu/precise --name=xnoise --version=0.2.6 --src-tar=xnoise-0.2.6.tar.gz
+  ./package_chef.sh --distro=debian --name=xnoise --version=0.2.5 --src-tar=xnoise-0.2.5.tar.gz
+  ./package_chef.sh --distro=ubuntu/precise --name=xnoise --version=0.2.5 --src-tar=xnoise-0.2.5.tar.gz
   
+  Source package directory is current working directory(\$PWD).
+  
+  ./package_chef.sh --distro=debian --name=xnoise --version=0.2.5
+  ./package_chef.sh --distro=ubuntu/precise --name=xnoise --version=0.2.5
+  
+  Source package directory is 'foo/xnoise-0.2.5'.
+  
+  ./package_chef.sh --distro=debian --name=xnoise --version=0.2.5 --src-dir=foo/xnoise-0.2.5
+  ./package_chef.sh --distro=ubuntu/precise --name=xnoise --version=0.2.5 --src-dir=foo/xnoise-0.2.5
   
 Bugs:
   Please report bugs in this script on the xnoise bugtracker here:
@@ -96,12 +108,32 @@ formatted_version=$(echo "$version" | sed 's/-/./g')
 std_version=$(echo "$version" | cut -d'-' -f1)
 name_version=$name-$std_version
 package_dir=$output_dir/$name_version
+orig_tar_name=${name}_${formatted_version}.orig.tar.bz2
 
 # Extract/copy source directory.
 case $src_tar in
   -1 )
     echo "no source tarball was given. copy source directory."
-    cp -R -T "$src_dir" "$package_dir"
+    if [ "${output_dir:0:1}" == "/" ]; then
+      absolute_output_dir=$output_dir
+    else
+      absolute_output_dir=$PWD/$output_dir
+    fi
+    echo "absolute dir is $absolute_output_dir"
+    if [ "$src_dir" == "$PWD" ]; then
+      mkdir -p "$package_dir"
+      for i in `ls -a -1`
+      do
+        echo $i;
+        if [ $i != . ] && [ $i != .. ] && \
+          [ "$PWD/$i" != "$absolute_output_dir" ]; then
+          echo "detected"
+          cp -R $i $package_dir
+        fi
+      done
+    else
+      cp -R -T "$src_dir" "$package_dir"
+    fi
     ;;
   *tar )
     echo "tarball ends with tar."
@@ -124,7 +156,6 @@ cp -R "$package_dir/packaging/$distro/debian" "$package_dir"
 old_dir0=$PWD
 
 cd $output_dir
-orig_tar_name=${name}_${formatted_version}.orig.tar.bz2
 
 # Create a source package (tarball).
 tar -cvjf $orig_tar_name $name_version
@@ -155,13 +186,25 @@ if [ "$dont_sign_changes" == "yes" ]; then
   debuild_args=$(echo "$debuild_args-uc ")
 fi
 
-if [ "$include_orig" == "yes" ]; then
-  debuild_args=$(echo "$debuild_args-sa ")
+if [ "$dont_include_orig" == "yes" ]; then
+  include_orig=no
 fi
 
-command -v debuild > /dev/null && \
+if [ "$include_orig" == "yes" ]; then
+  debuild_args=$(echo "$debuild_args-sa ")
+else
+  debuild_args=$(echo "$debuild_args-sd ")
+fi
+
+have_debuild=yes
+command -v debuild > /dev/null || have_debuild=no
+if [ "$have_debuild" == "no" ]; then
+  echo "debuild command haven’t been found. Please install the devscripts package." 1>&2;
+  exit 1;
+fi
+
 debuild $debuild_args || \
-{ echo "debuild command haven’t been found. Please install the devscripts package."; \
+{ echo "debuild exited with error." 1>&2; \
 exit 1; }
 
 cd ..
